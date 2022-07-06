82646145947042a9d57646394b8959db4910f8c6
maruthi.akula <maruthi.akula@PUN-LAP-DEV105>
2022-07-06 10:44:19 +0530

    Add project files.

---
+++ method override/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_override
+{
+    public class Baseclass
+    {
+      public virtual void print()
+        {
+            Console.WriteLine("I'm a Base Class");
+        }
+    }
+    public class Derivedclass:Baseclass
+    {
+        public override void print()
+        {
+            Console.WriteLine("I'm a Derived Class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baseclass Obj = new Derivedclass();
+            Obj.print();
+
+
+        }
+    }
+}
