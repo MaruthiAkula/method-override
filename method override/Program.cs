20079ab0240c069615705dd8fae2a6d3b3748d99
maruthi.akula <maruthiraoakula22@gmail.com>
2022-07-06 12:51:31 +0530

    .

--- method override/Program.cs
+++ method override/Program.cs
@@ -10,7 +10,7 @@ namespace method_override
     {
       public virtual void print()
         {
-            Console.WriteLine("I'm a Base Class");
+            Console.WriteLine("I'm a Base Classs");
         }
     }
     public class Derivedclass:Baseclass
